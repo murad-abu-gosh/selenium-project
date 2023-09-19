eda69e09477264a013605b943f762653f559c8ef
Murad Abu-Gosh <muradabgosh@gmail.com>
2023-09-19 19:50:35 +0300

    Fixed access for driver

--- src/main/java/POM/BaseComponent.java
+++ src/main/java/POM/BaseComponent.java
@@ -11,7 +11,7 @@ import java.time.Duration;
 
 public class BaseComponent {
 
-    private WebDriver driver;
+    protected WebDriver driver;
     public BaseComponent(WebDriver driver) {
         this.driver = driver;
 
