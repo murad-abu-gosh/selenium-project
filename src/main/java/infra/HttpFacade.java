62ab304da13a8a8387a0d7104a5bce25a3e7336d
Murad Abu-Gosh <muradabgosh@gmail.com>
2023-10-01 17:01:24 +0300

    update HttpFacade

--- src/main/java/infra/HttpFacade.java
+++ src/main/java/infra/HttpFacade.java
@@ -29,82 +29,68 @@ public class HttpFacade {
     public static <T> WrappHttpResponse<T> makeHttpRequest(Class<T> clz, String URL, Map<String, String> headers, HttpMethod method, String requestBody) {
         String result = null;
         CloseableHttpResponse response = null;
-
-        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-            if (method == HttpMethod.GET) {
-                HttpGet request = new HttpGet(URL);
-                response = httpClient.execute(request);
-            } else if (method == HttpMethod.POST) {
-
-                HttpPost request = new HttpPost(URL);
-//                System.out.println("1212"+request.getHeader("Content-Type")+"\n"+request.containsHeader("Content-Type"));
+        HttpUriRequestBase request = null;
+        switch (method) {
+            case GET -> request = new HttpGet(URL);
+            case POST -> {
+                request = new HttpPost(URL);
                 ;
                 if (headers != null) {
                     for (Map.Entry<String, String> header : headers.entrySet()) {
                         request.addHeader(header.getKey(), header.getValue());
                     }
                 }
-                System.out.println("HTTPfacade ==="+requestBody);
-//                StringEntity requestEntity = new StringEntity(requestBody, ContentType.parse("UTF-8"));
-//                request.setEntity(requestEntity);
-                System.out.println("Entity--------- "+requestBody.toString());
+                System.out.println("HTTPfacade ===" + requestBody);
+                System.out.println("Entity--------- " + requestBody.toString());
                 request.setEntity(new StringEntity(requestBody));
-                response = httpClient.execute(request);
-//                response.setHeader("Content-Type", "application/json; charset=UTF-8");
-
-            } else if (method == HttpMethod.PATCH) {
-
-                HttpPatch request = new HttpPatch(URL);
+            }
+            case PATCH -> {
+                request = new HttpPatch(URL);
                 request.setHeader("accept", "application/json");
                 request.setHeader("Content-Type", "application/json");
-
                 request.setEntity(new StringEntity(requestBody));
-                response = httpClient.execute(request);
-
-            } else if (method == HttpMethod.DELETE) {
-
-                HttpDelete request = new HttpDelete(URL);
-                response = httpClient.execute(request);
-
-            } else if (method == HttpMethod.PUT) {
-                HttpPut request = new HttpPut(URL);
+            }
+            case DELETE -> request = new HttpDelete(URL);
+            case PUT -> {
+                request = new HttpPut(URL);
                 if (headers != null) {
                     for (Map.Entry<String, String> header : headers.entrySet()) {
                         request.addHeader(header.getKey(), header.getValue());
                     }
                 }
                 request.setEntity(new StringEntity(requestBody));
-                response = httpClient.execute(request);
-            }
-
-            HttpEntity entity = response.getEntity();
-//            result = EntityUtils.toString(entity, "UTF-8");
-
-            result = EntityUtils.toString(entity);
-        } catch (IOException | ParseException e) {
-            throw new RuntimeException(e);
-        }  finally {
-            if (response != null) {
-                try {
-                    response.close();
-                } catch (IOException e) {
-                    // Handle the exception if necessary
-                }
             }
         }
 
 
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            response = httpClient.execute(request);
+            HttpEntity entity = response.getEntity();
+            result = EntityUtils.toString(entity);
+        } catch (IOException | ParseException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (response != null) {
+                try {
+                    response.close();
+                } catch (IOException e) {
+                    System.err.println("Unable to close response.");
+                    e.printStackTrace();
+                }
+            }
+        }
+
         Gson gson = new Gson();
         try {
 
-            System.out.println("status" +  response.getCode());
+            System.out.println("status" + response.getCode());
             System.out.println("response" + result);
-            if(result.isEmpty()){
-                result ="{}";
+            if (result.isEmpty()) {
+                result = "{}";
             }
             JSONObject jsonObject = new JSONObject(result);
 
-            return new WrappHttpResponse(response.getCode(), null, gson.fromJson(result, clz));
+            return new WrappHttpResponse<>(response.getCode(), null, gson.fromJson(result, clz));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
